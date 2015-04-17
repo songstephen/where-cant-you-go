6a1d19d0c669d35deb775a2d3a7a62f3d2d19d46
Stephen Song <stephen.song@gatech.edu>
2015-04-17 14:56:11 -0400

    Added points within O4W, Southeastern, and Hapeville

--- maps.js
+++ maps.js
@@ -327,6 +327,276 @@ function initialize() {
           coords: [33.757530, -84.315093],
           neighborhood: 'Edgewood',
           time: 52
+        },
+        {
+          coords: [33.756759, -84.371000],
+          neighborhood: 'Old Fourth Ward',
+          time: 32
+        },
+        {
+          coords: [33.754653, -84.375507],
+          neighborhood: 'Old Fourth Ward',
+          time: 38
+        },
+        {
+          coords: [33.757954, -84.360658],
+          neighborhood: 'Old Fourth Ward',
+          time: 31
+        },
+        {
+          coords: [33.758560, -84.354542],
+          neighborhood: 'Old Fourth Ward',
+          time: 18
+        },
+        {
+          coords: [33.748088, -84.375657],
+          neighborhood: 'Old Fourth Ward',
+          time: 26
+        },
+        {
+          coords: [33.749301, -84.356731],
+          neighborhood: 'Old Fourth Ward',
+          time: 44
+        },
+        {
+          coords: [33.754689, -84.368147],
+          neighborhood: 'Old Fourth Ward',
+          time: 33
+        },
+        {
+          coords: [33.745519, -84.366301],
+          neighborhood: 'Old Fourth Ward',
+          time: 38
+        },
+        {
+          coords: [33.738381, -84.384111],
+          neighborhood: 'Grant Park',
+          time: 53
+        },
+        {
+          coords: [33.739167, -84.366602],
+          neighborhood: 'Grant Park',
+          time: 68
+        },
+        {
+          coords: [33.731815, -84.354414],
+          neighborhood: 'Grant Park',
+          time: 68
+        },
+        {
+          coords: [33.724105, -84.365314],
+          neighborhood: 'Grant Park',
+          time: 67
+        },
+        {
+          coords: [33.716109, -84.357504],
+          neighborhood: 'Grant Park',
+          time: 99
+        },
+        {
+          coords: [33.717395, -84.371580],
+          neighborhood: 'Grant Park',
+          time: 69
+        },
+        {
+          coords: [33.726461, -84.384712],
+          neighborhood: 'Grant Park',
+          time: 60
+        },
+        {
+          coords: [33.733956, -84.385656],
+          neighborhood: 'Grant Park',
+          time: 54
+        },
+        {
+          coords: [33.733742, -84.375185],
+          neighborhood: 'Grant Park',
+          time: 56
+        },
+        {
+          coords: [33.725247, -84.376901],
+          neighborhood: 'Grant Park',
+          time: 68
+        },
+        {
+          coords: [33.730458, -84.364885],
+          neighborhood: 'Grant Park',
+          time: 62
+        },
+        {
+          coords: [33.723463, -84.352440],
+          neighborhood: 'Grant Park',
+          time: 67
+        },
+        {
+          coords: [33.715039, -84.387201],
+          neighborhood: 'Southeastern',
+          time: 76
+        },
+        {
+          coords: [33.710826, -84.378446],
+          neighborhood: 'Southeastern',
+          time: 59
+        },
+        {
+          coords: [33.705543, -84.386428],
+          neighborhood: 'Southeastern',
+          time: 100
+        },
+        {
+          coords: [33.705043, -84.399475],
+          neighborhood: 'Southeastern',
+          time: 66
+        },
+        {
+          coords: [33.695117, -84.394153],
+          neighborhood: 'Southeastern',
+          time: 105
+        },
+        {
+          coords: [33.693316, -84.379241],
+          neighborhood: 'Southeastern',
+          time: 69
+        },
+        {
+          coords: [33.700243, -84.356153],
+          neighborhood: 'Southeastern',
+          time: 82
+        },
+        {
+          coords: [33.694959, -84.363534],
+          neighborhood: 'Southeastern',
+          time: 84
+        },
+        {
+          coords: [33.699457, -84.380615],
+          neighborhood: 'Southeastern',
+          time: 68
+        },
+        {
+          coords: [33.702599, -84.396236],
+          neighborhood: 'Southeastern',
+          time: 69
+        },
+        {
+          coords: [33.706312, -84.374435],
+          neighborhood: 'Southeastern',
+          time: 66
+        },
+        {
+          coords: [33.705741, -84.361474],
+          neighborhood: 'Southeastern',
+          time: 71
+        },
+        {
+          coords: [33.692673, -84.391515],
+          neighborhood: 'Southeastern',
+          time: 62
+        },
+        {
+          coords: [33.686746, -84.393146],
+          neighborhood: 'Southeastern',
+          time: 102
+        },
+        {
+          coords: [33.684246, -84.384305],
+          neighborhood: 'Southeastern',
+          time: 84
+        },
+        {
+          coords: [33.691888, -84.371259],
+          neighborhood: 'Southeastern',
+          time: 96
+        },
+        {
+          coords: [33.686703, -84.357051],
+          neighborhood: 'Southeastern',
+          time: 77
+        },
+        {
+          coords: [33.683275, -84.368381],
+          neighborhood: 'Southeastern',
+          time: 92
+        },
+        {
+          coords: [33.679791, -84.404719],
+          neighborhood: 'Hapeville',
+          time: 66
+        },
+        {
+          coords: [33.672077, -84.400943],
+          neighborhood: 'Hapeville',
+          time: 76
+        },
+        {
+          coords: [33.676291, -84.389527],
+          neighborhood: 'Hapeville',
+          time: 62
+        },
+        {
+          coords: [33.670505, -84.392789],
+          neighborhood: 'Hapeville',
+          time: 95
+        },
+        {
+          coords: [33.677505, -84.393561],
+          neighborhood: 'Hapeville',
+          time: 108
+        },
+        {
+          coords: [33.673077, -84.384635],
+          neighborhood: 'Hapeville',
+          time: 67
+        },
+        {
+          coords: [33.679791, -84.377683],
+          neighborhood: 'Hapeville',
+          time: 91
+        },
+        {
+          coords: [33.671362, -84.378627],
+          neighborhood: 'Hapeville',
+          time: 74
+        },
+        {
+          coords: [33.670862, -84.367383],
+          neighborhood: 'Hapeville',
+          time: 82
+        },
+        {
+          coords: [33.668005, -84.379571],
+          neighborhood: 'Hapeville',
+          time: 68
+        },
+        {
+          coords: [33.671434, -84.390986],
+          neighborhood: 'Hapeville',
+          time: 111
+        },
+        {
+          coords: [33.659432, -84.391930],
+          neighborhood: 'Hapeville',
+          time: 92
+        },
+        {
+          coords: [33.648358, -84.386008],
+          neighborhood: 'Hapeville',
+          time: 104
+        },
+        {
+          coords: [33.658647, -84.380343],
+          neighborhood: 'Hapeville',
+          time: 70
+        },
+        {
+          coords: [33.653574, -84.374249],
+          neighborhood: 'Hapeville',
+          time: 79
+        },
+        {
+          coords: [33.658004, -84.370559],
+          neighborhood: 'Hapeville',
+          time: 84
         }
       ];
 
